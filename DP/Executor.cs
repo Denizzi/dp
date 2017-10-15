75d76f01285b60e47efd0bb3004bd60f737eb634
Deniz <yamandnz@gmail.com>
2017-10-16 01:24:48 +0300

    Decorator class diagram added

--- DP/Executor.cs
+++ DP/Executor.cs
@@ -23,19 +23,6 @@ namespace DP
     {
         internal static void ExecuteDecorator()
         {
-            //Intent
-            //  • Attach additional responsibilities to an object dynamically.
-            //      Decorators provide a flexible alternative to subclassing for extending
-            //      functionality.
-            //  • Client - specified embellishment of a core object by recursively
-            //      wrapping it.
-            //  • Wrapping a gift, putting it in a box, and wrapping the box.
-
-            //Problem
-            //  You want to add behavior or state to individual objects at run-time.
-            //  Inheritance is not feasible because it is static and applies to an entire
-            //  class.
-
             Beverage beverage = new Cream(new Cream(new Espresso()));
             Beverage beverage2 = new Mocha(new Milk(new Americano()));
 
